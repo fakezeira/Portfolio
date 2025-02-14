c0cd2bfa5daff98b1bb1e5a937264ddab92d059c
fakezeira <joaojp.araujo@gmail.com>
2025-02-14 00:18:40 -0300

    Adicionando uma nova seção para o site (seção de contato).

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navBar.classList.toggle('ativo');
 }
+
+// Função de confirmação
+function confirmacao() {
+    const mensagemConfirmacao = document.getElementById('mensagemConfirmacao');
+    mensagemConfirmacao.style.display = 'inline-block';
+    mensagemConfirmacao.innerHTML = 'Sua mensagem foi encaminhada com sucesso!';
+
+    setTimeout(function() {
+        mensagemConfirmacao.style.display = 'none';
+    }, 2000);  
+}
